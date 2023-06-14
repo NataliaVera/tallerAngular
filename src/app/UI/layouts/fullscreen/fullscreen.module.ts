7b8e60d57df464d3b11ae76d3099162ebee7c64e
unknown <nataliavera2100@gmail.com>
2023-06-13 19:03:16 -0500

    Segunda clase: estructura de proyecto y vistas

---
+++ src/app/UI/layouts/fullscreen/fullscreen.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FullscreenComponent } from './fullscreen.component';
+
+
+//Administrador de todos los componentes o vistas
+@NgModule({
+  declarations: [
+    FullscreenComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FullscreenModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultModule } from './UI/layouts/default/default.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DefaultModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
